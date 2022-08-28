438ef3e769f9c630f8e82d4cae65a1e019c4b594
dieantu <whoami@dieantu.com>
2022-08-28 21:31:31 +0200

    edit

--- installer.sh
+++ installer.sh
@@ -9,6 +9,8 @@ curl --tlsv1.3 --proto =https --max-time 180 --output ~/derivative.asc https://w
 cp ~/derivative.asc /usr/share/keyrings/derivative.asc
 apt install apt-transport-tor
 echo "deb [signed-by=/usr/share/keyrings/derivative.asc] tor+http://deb.w5j6stm77zs6652pgsij4awcjeel3eco7kvipheu6mtr623eyyehj4yd.onion bullseye main contrib non-free" | tee /etc/apt/sources.list.d/derivative.list
+apt update
+apt full-upgrade
 apt install --no-install-recommends kicksecure-cli
 mv /etc/apt/sources.list ~/
 touch /etc/apt/sources.list
