6d408f7822963afc2b182ae7ff185c8401874936
dieantu <whoami@dieantu.com>
2022-08-29 05:30:28 +0200

    added auto delete of gnome games

--- installer.sh
+++ installer.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 apt update
-apt full-upgrade
-apt install --no-install-recommends sudo adduser
+apt full-upgrade -y
+tasksel install desktop gnome-desktop
+apt remove gnome-games -y
+apt install --no-install-recommends sudo adduser -y
 /usr/sbin/addgroup --system console
 /usr/sbin/adduser user console
-apt install --no-install-recommends curl
+apt install --no-install-recommends curl -y
 curl --tlsv1.3 --proto =https --max-time 180 --output ~/derivative.asc https://www.kicksecure.com/derivative.asc
 cp ~/derivative.asc /usr/share/keyrings/derivative.asc
-apt install apt-transport-tor
+apt install apt-transport-tor -y
 echo "deb [signed-by=/usr/share/keyrings/derivative.asc] tor+http://deb.w5j6stm77zs6652pgsij4awcjeel3eco7kvipheu6mtr623eyyehj4yd.onion bullseye main contrib non-free" | tee /etc/apt/sources.list.d/derivative.list
 apt update
-apt full-upgrade
-apt install --no-install-recommends kicksecure-cli
+apt full-upgrade -y
+apt install --no-install-recommends kicksecure-cli -y
 mv /etc/apt/sources.list ~/
 touch /etc/apt/sources.list
+# tasksel install laptop
+systemctl set-default graphical
